31f3f7b59cf8cccff4fca1d41578104551ef54d6
T Prayag <prayagtushar2016@gmail.com>
2025-02-01 17:34:21 +0530

    chore: set up server.js with Express and initial configurations

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ connectDB();
 
 app.use('/api/faqs', faqRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
